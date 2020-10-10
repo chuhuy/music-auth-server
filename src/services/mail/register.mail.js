396be92264a76d141bc091008f4a90305e09b7a7
chuhuy <45164683+chuhuy@users.noreply.github.com>
2020-10-11 00:16:37 +0700

    Add mail template

--- src/services/mail/register.mail.js
+++ src/services/mail/register.mail.js
@@ -1,8 +1,20 @@
 const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const registerMail = (email) => {
     const token = jwt.sign({ mail: email }, process.env.MAIL_TOKEN_SECRET, { expiresIn: '1h' });
+
+    const readHTMLFile = function(path, callback) {
+        fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+            if (err) {
+                throw err;
+            }
+            else {
+                callback(null, html);
+            }
+        });
+    };
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -15,7 +27,8 @@ const registerMail = (email) => {
         from: "Music Life <noreply.musiclife@gmail.com>",
         to: email,
         subject: 'Validate your email',
-        text: `Please validate your email with the following link: http://${process.env.SERVER_HOST}/api/v1/user/validate?email=${email}&token=${token}`
+        text: `Please validate your email with the following link: http://${process.env.SERVER_HOST}/api/v1/user/validate?email=${email}&token=${token}`,
+        html: `<div style="width: 640px; text-align: center; margin: auto; padding: 10px 20px 25px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"><h1 style="margin: 0; font-size: 30px;">Welcome to Life Music App</h1><p style="margin: 24px 0 30px; text-align: left;">Thank you for creating an account in my app. To complete the registration process, please click this button below to verify your e-mail address.</p><a href="http://${process.env.SERVER_HOST}/api/v1/user/validate?email=${email}&token=${token}"><button style=" background-color: #000A19; color: #fff; border: none; padding: 16px 25px;   font-size: 16px; cursor: pointer; border-radius: 4px;">Verify your e-mail</button></a><p style="margin: 30px 0 0; text-align: left;">Welcome to Life Music Community!</p><p style=" margin: 6px 0 0; text-align: left; font-size: 13px;">The Life Music Team</p></div>`
     };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
--- src/services/mail/reset-password.mail.js
+++ src/services/mail/reset-password.mail.js
@@ -19,7 +19,8 @@ const resetPasswordMail = (email, password) => {
             from: "Music Life <noreply.musiclife@gmail.com>",
             to: email,
             subject: 'Reset password',
-            text: `Your new password is: ${password}. Please click the following link to reset your password: http://${process.env.SERVER_HOST}/api/v1/user/reset?s=${hash}&e=${email}&t=${token}`
+            text: `Your new password is: ${password}. Please click the following link to reset your password: http://${process.env.SERVER_HOST}/api/v1/user/reset?s=${hash}&e=${email}&t=${token}`,
+            html: `<div style="width: 600px; margin: auto; padding: 10px 20px 25px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"><h1 style="margin: 0; font-size: 30px">Hi,</h1><p style="margin: 24px 0 30px">You recently requested to reset your password for your Life Music account. Your new password is: <strong>${password}</strong>, please click the button below to reset it.</p><div style="text-align: center; padding: 16px 25px;"><a href="http://${process.env.SERVER_HOST}/api/v1/user/reset?s=${hash}&e=${email}&t=${token}" style=" background-color: #000A19; color: #fff; margin: 30px 0; padding: 16px 25px; font-size: 16px; cursor: pointer; text-decoration: none; border-radius: 4px;">Reset your password</a></div><p style="margin: 30px 0 0">If you didn't request a password reset, please ignore this email. The password reset is only valid for the next 20 minutes.</p><p style="margin: 24px 0 0">Thanks,</p><p style="margin: 6px 0 0; font-size: 13px">The Life Music Team</p></div>`
         };
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
